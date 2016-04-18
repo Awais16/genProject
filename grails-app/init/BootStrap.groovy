a6cf2fddca4ae7a8d3fc586e8044ac293c37760f
Muhammad Awais Akhtar <awaisakhtar16@yahoo.com>
2016-04-19 00:15:12 +0200

    spring security core plugin

--- grails-app/init/BootStrap.groovy
+++ grails-app/init/BootStrap.groovy
@@ -1,6 +1,24 @@
+import com.ikmb.SecRole
+import com.ikmb.SecUser
+import com.ikmb.SecUserSecRole
 class BootStrap {
 
     def init = { servletContext ->
+    	def adminRole = new SecRole('ROLE_ADMIN').save()
+    	def participantRole = new SecRole('ROLE_PARTICIPANTS').save()
+    	def staffRole = new SecRole('ROLE_STAFF').save()
+
+		def adminUser = new SecUser('admin', 'admin').save()
+		def staffUser = new SecUser('staff', 'staff').save()
+		def participantUser = new SecUser('user', 'user').save()
+
+		SecUserSecRole.create adminUser, adminRole
+		SecUserSecRole.create staffUser, staffRole
+		SecUserSecRole.create participantUser, participantRole
+		SecUserSecRole.withSession {
+			it.flush()
+			it.clear()
+    	}
     }
     def destroy = {
     }
